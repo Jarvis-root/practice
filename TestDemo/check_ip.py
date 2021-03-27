2deaba9519e8b52dabb7767e3b00cad48feb1b56
Jarvis-root <qh78945@qq.com>
2021-03-27 22:38:28 +0800

    update

---
+++ TestDemo/check_ip.py
@@ -0,0 +1,47 @@
+import pytest
+
+
+def check_ip(ip: str):
+    if not isinstance(ip, str):
+        return False
+    if '.' not in ip:
+        return False
+    str_list = ip.split('.')
+    count = 0
+    if len(str_list) == 4:
+        for s in str_list:
+            if s.isdigit():
+                if '0' <= s <= '255':
+                    if len(s) > 1:
+                        if not s.startswith('0'):
+                            count += 1
+                    else:
+                        count += 1
+    return count == 4
+
+
+@pytest.mark.parametrize('s', [
+    '',
+    'a',
+    '200',
+    '256.256.256.256',
+    '25,6.25!6.256.25/6',
+    '-1.-1.-1.-1',
+    '!!!'
+])
+def test_fail(s):
+    assert not check_ip(s)
+
+
+@pytest.mark.parametrize('s', [
+    '255.255.255.255',
+    '254.254.254.254',
+    '0.0.0.0',
+    '127.0.0.1',
+])
+def test_suc(s):
+    assert check_ip(s)
+
+
+if __name__ == '__main__':
+    pytest.main()
